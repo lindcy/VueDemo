8b8476d460ec0c256ae1951e453869ca8d4680e9
panda <930105355@qq.com>
2021-03-25 18:37:54 +0800

    DemoHtml

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+html,body{
+width:100%;
+height:100%;
+padding:0px;
+margin:0px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  height:100%;
+  width:100%;
+  padding:0px;
+  margin:0px;  
+}
+</style>
---
+++ src/components/Demo.vue
@@ -0,0 +1,90 @@
+<template>
+<div>
+  <div class="block">
+    <el-carousel height="150px">
+      <el-carousel-item v-for="item in banner" :key="item">
+        <img class="titlecd" :src="item.img"/>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+  <div>
+    <el-row v-for="item in posts" :key="item">
+  <el-col class="picPath"><div class="grid-content bg-purple"><img class="pathcd" :src="item.cover"/></div></el-col>
+  <el-col class="picTitle"><div class="grid-content bg-purple-light contantcd">{{item.title}}</div></el-col>
+</el-row>
+  </div>
+</div>
+</template>
+<style>
+  .el-carousel__item:nth-child(2n) {
+     background-color: #99a9bf;
+  }
+  .el-carousel__item:nth-child(2n+1) {
+     background-color: #d3dce6;
+  }
+   .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+.pathcd{
+    display: block!important;
+    width: 100%!important;
+    height: 200px;
+  }
+  .contantcd{
+      height: 200px!important;  
+      text-align: left;
+      padding:0px 0px 0px 10px;
+  }
+  .titlecd,.block,.el-carousel__container{
+    height: 350px!important;
+    width: 100%;
+    display: block;
+  }
+  .el-row{
+    margin-bottom:10px!important;
+  }
+  .picPath{
+    width: 300px;
+  }
+    .picTitle{
+    width: calc(100% - 300px);
+  }
+</style>
+
+<script>
+ export default {
+  name: "Bannere",
+  data() {
+   return {
+    banner:[],
+    posts:[]
+    };
+  },
+  mounted() {
+    this.$axios.get('http://192.168.89.72:81/v1/mock/page').then(response => {
+          this.info = response.data.data;
+          this.banner=this.info.banner;
+          this.posts=this.info.posts;
+      })
+  }
+};
+</script>
